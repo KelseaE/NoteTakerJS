dfd6626a2b0f00555b7e937f8ae4f63be18c5abe
KEDell <sosha98@live.com>
2024-02-10 13:26:38 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = 3002;
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.json())
+
+const api = require('./routes/index')
+
+
+app.use('/api', api)
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`))
